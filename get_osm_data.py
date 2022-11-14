8ab7268cbb21676b2498f30f9570a76d41fb0678
thomasmaliappis <thomasmaliappis@gmail.com>
2022-11-14 17:57:57 +0100

    added some TODOs

---
+++ get_osm_data.py
@@ -0,0 +1,2 @@
+# TODO get OpenStreetMap data for each image
+# https://stackoverflow.com/questions/69174361/how-to-extract-street-graph-or-network-from-openstreetmap/69377396
--- resize_images.py
+++ resize_images.py
@@ -6,3 +6,8 @@ image = Image.open(path)
 
 new_image = image.resize((512, 512))
 new_image.save('myimage_512.jpg')
+
+# TODO create function that resizes all images in 128, 256 and 512 sizes
+# does not only need resize but also cropping for zoom purposes
+# concatenate data before
+# stack data after
