9b3d1d8505e257a30525e11fdf6a6654b7d2e910
thomasmaliappis <thomasmaliappis@gmail.com>
2022-11-14 18:18:16 +0100

    added get street network image from coordinates and distance

--- get_osm_data.py
+++ get_osm_data.py
@@ -1,2 +1,6 @@
 # TODO get OpenStreetMap data for each image
 # https://stackoverflow.com/questions/69174361/how-to-extract-street-graph-or-network-from-openstreetmap/69377396
+import osmnx as ox
+
+G = ox.graph_from_point((37.88915, 23.43844), dist=750, network_type='all')
+ox.plot_graph(G, save=True, filepath='test.jpg')
