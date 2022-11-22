8428dbeefcf0f51b100be015936d85efbe6fa542
thomasmaliappis <thomasmaliappis@gmail.com>
2022-11-22 15:09:09 +0100

    another failed attempt with OSM data

--- get_osm_data.py
+++ get_osm_data.py
@@ -1,96 +1,36 @@
-import math
 import osmnx as ox
-
-# TODO get OpenStreetMap data for each image
-# https://stackoverflow.com/questions/69174361/how-to-extract-street-graph-or-network-from-openstreetmap/69377396
-
-# TODO check what the below code does
-# if we can use it to get the coordinates of the corners of the image
-# and then ask for the network data using the bounding box they represent
-
-TileSize = 128
-OriginX =  TileSize / 2
-OriginY =  TileSize / 2
-PixelsPerLonDegree = TileSize / 360.0
-PixelsPerLonRadian = TileSize / (2 * math.pi)
-
-
-def degrees_to_radians(deg):
-    return deg * math.pi / 180.0
-
-def radians_to_degrees(rads):
-    return rads * 180.0 / math.pi
-
-def bound(value, min_val, max_val):
-    value = min([value, max_val])
-    return max([value, min_val])
-
-# From Lat, Lon to World Coordinate X, Y. I'm being explicit in assigning to
-# X and Y properties.
-def mercator(latitude, longitude):
-    siny = bound(math.sin(degrees_to_radians(latitude)), -.9999, .9999)
-
-    x = OriginX + longitude*PixelsPerLonDegree
-    y = OriginY + .5 * math.log((1 + siny) / (1 - siny)) * -PixelsPerLonRadian
-
-    return x, y
-
-# //From World Coordinate X, Y to Lat, Lon. I'm being explicit in assigning to
-# //Latitude and Longitude properties.
-def inverse_mercator(x, y):
-
-    lon = (x - OriginX) / PixelsPerLonDegree
-    latRadians = (y - OriginY) / -PixelsPerLonRadian
-    lat = radians_to_degrees(math.atan(math.sinh(latRadians)))
-
-    return lat, lon
-
-
-def get_bounds(center, zoom, mapWidth, mapHeight):
-    scale = math.pow(2, zoom)
-
-    centerWorld = mercator(center[0], center[1])
-    centerPixel = (centerWorld[0] * scale, centerWorld[1] * scale)
-
-    NEPixel = (centerPixel[0] + mapWidth / 2.0, centerPixel[1] - mapHeight / 2.0)
-    SWPixel = (centerPixel[0] - mapWidth / 2.0, centerPixel[1] + mapHeight / 2.0)
-    # NWPixel = (centerPixel[0] - mapWidth / 2.0, centerPixel[1] - mapHeight / 2.0)
-    # SEPixel = (centerPixel[0] + mapWidth / 2.0, centerPixel[1] + mapHeight / 2.0)
-
-    NEWorld = (NEPixel[0] / scale, NEPixel[1] / scale)
-    SWWorld = (SWPixel[0] / scale, SWPixel[1] / scale)
-    # NWWorld = (NWPixel[0] / scale, NWPixel[1] / scale)
-    # SEWorld = (SEPixel[0] / scale, SEPixel[1] / scale)
-
-    NELatLon = inverse_mercator(NEWorld[0], NEWorld[1])
-    SWLatLon = inverse_mercator(SWWorld[0], SWWorld[1])
-    # NWLatLon = inverse_mercator(NWWorld[0], NWWorld[1])
-    # SELatLon = inverse_mercator(SEWorld[0], SEWorld[1])
-
-    # return NWLatLon, NELatLon, SELatLon, SWLatLon
-    return NELatLon, SWLatLon
+from PIL import Image
 
 if __name__ == "__main__":
-    
-    # # get graph frome center and distance
-    # G = ox.graph_from_point((37.88915, 23.43844), dist=500, network_type='all')
-    # ox.plot_graph(G, save=True, filepath='test.jpg')
 
-    # result = get_bounds((37.88915, 23.43844), 17, 512, 512)
-    # north = result[0][0]
-    # south = result[1][0]
-    # east = result[0][1]
-    # west = result[1][1]
+    file_name = 'test'
+    extension = '.jpg'
+    file_path = file_name + extension
+    # get graph frome center and distance
+    G = ox.graph_from_point((37.8891505835, 23.4384444463), dist=500, network_type='all')
+    ox.plot_graph(G, show=False, save=True, filepath=file_path)
 
-    # print(result)
+    image = Image.open(file_path)
+    image_resized_128 = image.resize((512, 512))
 
-    # # get graph from bounding box
-    # G = ox.graph_from_bbox(north, south, east, west, network_type='all')
-    # G_projected = ox.project_graph(G)
-    # ox.plot_graph(G_projected)
+    file_path = file_name + '_resized_512' + extension
+    image_resized_128.save(file_path)
 
-    import srtm
-    geo_elevation_data = srtm.get_data()
-    image = geo_elevation_data.get_image((500, 500), (45, 46), (13, 14), 300)
-    # the image s a standard PIL object, you can save or show it:
-    image.show()
+    from PIL import Image
+    #Read the two images
+    network_image = Image.open(file_path)
+    network_image.show()
+    original_image_file_path = 'extracted-data\imagery\\athina, gr\Agricultural + Semi-natural areas + Wetlands\\athina_z17_s512_37.8891505835_23.4384444463_grid-i0_grid-j23.jpg'
+    satellite_image = Image.open(original_image_file_path)
+    satellite_image.show()
+    #resize, first image
+    # network_image = network_image.resize((426, 240))
+    network_image_size = network_image.size
+    satellite_image_size = satellite_image.size
+    merged_image = Image.new('RGB',(network_image_size[0], network_image_size[1]), (250,250,250))
+    merged_image.paste(network_image,(0,0))
+    merged_image.paste(satellite_image,(0,0))
+    final_file_path = "extracted-data/merged/test_merged_image.jpg"
+    image_type = "JPEG"
+    merged_image.save(final_file_path, image_type)
+    merged_image.show()
